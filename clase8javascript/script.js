76994b3ceefc1f8788d7a3f27c8bcd5e79b56a73
Federico <federicobatalla@tdf.edu.ar>
2022-07-29 18:55:03 -0300

    ejercicio terminado

--- clase8javascript/script.js
+++ clase8javascript/script.js
@@ -1,30 +1,36 @@
 "use sctrict";
 
 let btnTirarDados = document.getElementById("btnTirarDados");
-btnTirarDados.addEventListener("click",tirarDados);
-
+btnTirarDados.addEventListener("click",informe);
 
 
 
 
 function tirarDados(){
-
-    let cantidad7 = 0;
-
-for (let i = 0; i < 1000; i++) {
-    let numeroAzar=(Math.trunc(Math.random()*10));
-    
-    
-    if (numeroAzar == 7) {
-        cantidad7++
-    }
-    
-    
+    let dado=(Math.floor(Math.random()*(7-1)+1));
+    return dado;
 }
 
-console.log(cantidad7);
 
 
+
+function informe(){
+
+let cantidad7 = 0;
+let dado1 = 0;
+let dado2 = 0;
+let sumaDosDados = 0;
+for (let i = 0; i < 1000; i++) {
+    dado1= tirarDados();
+    dado2= tirarDados();
+    sumaDosDados = dado1 + dado2 
+    
+    if (sumaDosDados == 7) {
+        cantidad7++
+    }
+}
+
+console.log("Cantidad de numeros 7 Salidos : "+cantidad7);
 
 }
 

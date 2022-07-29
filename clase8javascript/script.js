02fb037af34425c1cf9ca890eecb91bfb2caeb62
Federico <federicobatalla@tdf.edu.ar>
2022-07-29 18:54:53 -0300

    terminado

--- clase8javascript/script.js
+++ clase8javascript/script.js
@@ -9,5 +9,22 @@ btnTirarDados.addEventListener("click",tirarDados);
 
 function tirarDados(){
 
+    let cantidad7 = 0;
+
+for (let i = 0; i < 1000; i++) {
+    let numeroAzar=(Math.trunc(Math.random()*10));
+    
+    
+    if (numeroAzar == 7) {
+        cantidad7++
+    }
+    
+    
+}
+
+console.log(cantidad7);
+
+
+
 }
 
